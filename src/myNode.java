d0e86cc089be7dd05f33995f33f9884578541f3c
Adiel1892 <adiel1892@gmail.com>
2021-11-23 20:27:18 +0200

    classes

--- src/myNode.java
+++ src/myNode.java
@@ -1,19 +1,43 @@
 import api.GeoLocation;
 import api.NodeData;
 
+import java.util.HashMap;
+
 public class myNode implements NodeData {
     private static int id = 0;
     private int key;
     private GeoLocation location;
     private String info;
     private int tag;
+    private HashMap<Integer, myNode> neighbors;
+    private HashMap<Integer, myNode> edges;
 
-    public myNode(){
+    public myNode() {
         this.key = id++;
         this.info = null;
         this.tag = 0;
         this.location = new myGeo();
+        this.neighbors = new HashMap<>();
+        this.edges = new HashMap<>();
+    }
 
+    public myNode(int key) {
+        this.key = key;
+        this.info = null;
+        this.tag = 0;
+        this.location = new myGeo();
+        this.neighbors = new HashMap<>();
+        this.edges = new HashMap<>();
+    }
+    public myNode (myNode other){
+        if (other != null){
+            this.key = other.key;
+            this.info = other.info;
+            this.tag = other.tag;
+            this.location = other.location;
+            this.neighbors = new HashMap<>();
+            this.edges = new HashMap<>();
+        }
     }
 
     @Override
